8e8ed549edf30d97c9f52e9eb20605366f5e40ce
Michal Kvasnička <prgosek@gmail.com>
2021-07-30 15:06:36 +0200

    přidání předpon balíků

--- R/stargate_basics.R
+++ R/stargate_basics.R
@@ -43,26 +43,26 @@ sg_model <- function(m, name, ...)
 sg_model.default <- function(m, name = "") {
     if (inherits(m, "stargate"))
         return(m)
-    coefs <- tidy(m)
-    stats <- glance(m) |>
-        mutate(across(everything(), list)) |>
-        pivot_longer(everything(), names_to = "stat", values_to = "val")
-    name <- tibble(name = name)
-    mod <- list(coefs = coefs, stats = stats, name = name)
-    class(mod) <- c("sg_model", "stargate")
-    mod
+    coefs <- broom::tidy(m)
+    stats <- broom::glance(m) |>
+        dplyr::mutate(dplyr::across(dplyr::everything(), list)) |>
+        tidyr::pivot_longer(dplyr::everything(),
+                            names_to = "stat", values_to = "val")
+    name <- tibble::tibble(name = name)
+    structure(list(coefs = coefs, stats = stats, name = name),
+              class = c("sg_model", "stargate"))
 }
 
 
 #' @export
 sg_table <- function(...) {
     count_models_in_tables <- function(x) {
-        count <- map_int(x,
-                         ~ case_when(
-                             inherits(., "sg_model") ~ 0L,
-                             inherits(., "sg_table") ~ length(unique(.$name$model_id)),
-                             TRUE ~ -1L
-                         ))
+        count <- purrr::map_int(x,
+                                ~ dplyr::case_when(
+                                    inherits(., "sg_model") ~ 0L,
+                                    inherits(., "sg_table") ~ length(unique(.$name$model_id)),
+                                    TRUE ~ -1L
+                                ))
         if (any(count == -1L)) stop("OOOOOoooo!")
         sum(count)
     }
@@ -70,25 +70,24 @@ sg_table <- function(...) {
         {tab <- x[[var]]
         if (inherits(x, "sg_model"))
             tab <- tab |>
-                mutate(model_id = id) |>
-                select(model_id, everything())
+                dplyr::mutate(model_id = id) |>
+                dplyr::select(model_id, dplyr::everything())
         tab}
     }
     mods <- list(...)
-    mods <- map(mods, sg_model)
+    mods <- purrr::map(mods, sg_model)
     first_model_id <- count_models_in_tables(mods)
     model_ids <- first_model_id + mods |>
-        map_int(~class(.)[1] == "sg_model") |>
+        purrr::map_int(~class(.)[1] == "sg_model") |>
         cumsum()
-    coefs <- map2(mods, model_ids, ~add_model_id(.x, "coefs", .y)) |>
-        bind_rows()
-    stats <- map2(mods, model_ids, ~add_model_id(.x, "stats", .y)) |>
-        bind_rows()
-    name <- map2(mods, model_ids, ~add_model_id(.x, "name", .y)) |>
-        bind_rows()
-    tab <- list(coefs = coefs, stats = stats, name = name)
-    class(tab) <- c("sg_table", "stargate")
-    tab
+    coefs <- purrr::map2(mods, model_ids, ~add_model_id(.x, "coefs", .y)) |>
+        dplyr::bind_rows()
+    stats <- purrr::map2(mods, model_ids, ~add_model_id(.x, "stats", .y)) |>
+        dplyr::bind_rows()
+    name <- purrr::map2(mods, model_ids, ~add_model_id(.x, "name", .y)) |>
+        dplyr::bind_rows()
+    structure(list(coefs = coefs, stats = stats, name = name),
+              class = c("sg_table", "stargate"))
 }
 
 
@@ -112,13 +111,13 @@ sg_format_coefs <- function(x,
     starred <- function(p.value) {
         stars <- sort(stars)
         width <- max(length(names(stars)))
-        map(p.value, ~names(stars)[. <= stars][1]) |>
-            str_replace_na(replacement = "") |>
-            str_pad(width, side = "right")
+        purrr::map(p.value, ~names(stars)[. <= stars][1]) |>
+            stringr::str_replace_na(replacement = "") |>
+            stringr::str_pad(width, side = "right")
     }
 
     x$coefs <- x$coefs |>
-        mutate(formated_coefs = glue(format,
+        dplyr::mutate(formated_coefs = glue::glue(format,
                                      estimate = formatted(estimate, nsmall = nsmall, ...),
                                      std.error = formatted(std.error, nsmall = nsmall, ...),
                                      statistic = formatted(statistic, nsmall = nsmall, ...),
@@ -130,43 +129,43 @@ sg_format_coefs <- function(x,
 
 
 sg_format_stats <- function(x, nsmall = 2, ...) {
-    # pozor! formatted() je duplicitní s sg_format_coefs()
     x$stats <- x$stats |>
-        mutate(formatted_stats = map_chr(val,
-                                         ~case_when(
-                                             is.integer(.) ~ formatted(., nsmall = 0, ...),
-                                             is.double(.) ~ formatted(., nsmall = nsmall, ...),
-                                             TRUE ~ as.character(.)
-                                         )))
+        dplyr::mutate(formatted_stats = purrr::map_chr(val,
+                                                       ~dplyr::case_when(
+                                                           is.integer(.) ~ formatted(., nsmall = 0, ...),
+                                                           is.double(.) ~ formatted(., nsmall = nsmall, ...),
+                                                           TRUE ~ as.character(.)
+                                                       )))
     x
 }
 
 
 #' @export
 sg_format <- function(tab, nsmall = 2, ...) {
-    name <- if_else(tab$name$name == "",
-                    str_c("(", seq_along(tab$name$name), ")"),
-                    tab$name$name
+    name <- dplyr::if_else(tab$name$name == "",
+                           stringr::str_c("(", seq_along(tab$name$name), ")"),
+                           tab$name$name
     )
     get_together <- function(tab, name) {
         tab |>
-            select(1, 2, ncol(tab)) |>
-            group_by(model_id) |>
-            group_split(.keep = FALSE) |>
-            reduce(full_join, by = names(tab)[2]) |>
-            mutate(across(-1, ~str_replace_na(., replacement = ""))) |>
+            dplyr::select(1, 2, ncol(tab)) |>
+            dplyr::group_by(model_id) |>
+            dplyr::group_split(.keep = FALSE) |>
+            purrr::reduce(dplyr::full_join, by = names(tab)[2]) |>
+            dplyr::mutate(across(-1,
+                                 ~stringr::str_replace_na(., replacement = ""))) |>
             setNames(c("oo", name))
     }
     coefs <- tab |>
         sg_format_coefs(nsmall = nsmall, ...) |>
-        pluck("coefs") |>
+        purrr::pluck("coefs") |>
         get_together(name)
     stats <- tab |>
         sg_format_stats(nsmall = nsmall, ...) |>
-        pluck("stats") |>
+        purrr::pluck("stats") |>
         get_together(name)
     # res <- list(coefs = coefs, stats = stats)
     # class(res) <- c("sg_formated_table", "stargate")
     # res
-    bind_rows(coefs, stats)
+    dplyr::bind_rows(coefs, stats)
 }
